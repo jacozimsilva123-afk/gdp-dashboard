dd158bd41e0c3e322369218283ef23ce295d4c7c
jacozimsilva123-afk <jacozimsilva123@gmail.com>
2026-08-01 17:13:33 +0000

    codigo estreamlite final

--- app.py
+++ app.py
@@ -1,8 +1,33 @@
-import streamlit as st
+import tkinter as tk
+import webbrowser
 
-st.set_page_config(page_title="Meus Atalhos", layout="centered")
-st.title("🔗 Meus Atalhos")
+janela = tk.Tk()
+janela.title("Meus Atalhos")
+janela.geometry("300x250")
 
-st.link_button("Abrir Google", "https://google.com", use_container_width=True)
-st.link_button("Abrir YouTube", "https://youtube.com", use_container_width=True)
-st.link_button("Abrir WhatsApp Web", "https://web.whatsapp.com", use_container_width=True)
+
+def abrir_google():
+    webbrowser.open("https://google.com")
+
+
+def abrir_youtube():
+    webbrowser.open("https://youtube.com")
+
+
+def abrir_whatsapp():
+    webbrowser.open("https://web.whatsapp.com")
+
+
+titulo = tk.Label(janela, text="Meus Atalhos", font=("Arial", 20, "bold"))
+titulo.pack(pady=20)
+
+btn_google = tk.Button(janela, text="Abrir Google", command=abrir_google, width=20, height=2)
+btn_google.pack(pady=5)
+
+btn_youtube = tk.Button(janela, text="Abrir YouTube", command=abrir_youtube, width=20, height=2)
+btn_youtube.pack(pady=5)
+
+btn_whats = tk.Button(janela, text="Abrir WhatsApp", command=abrir_whatsapp, width=20, height=2)
+btn_whats.pack(pady=5)
+
+janela.mainloop()
